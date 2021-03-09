ea19c27ed5d024e0a8a6621fc86f45e70aca9efd
Richard <richards@aie.edu.au>
2021-03-09 11:37:14 +1100

    Start of math library

---
+++ MathLibrary/Vector2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MathLibrary
+{
+	public struct Vector2
+	{
+		public float x;
+		public float y;
+
+		//Constructors
+		public Vector2(float x = 0.0f, float y = 0.0f)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		//Operator overloading
+		//V = V + V(point translated by a vector)
+		public static Vector2 operator+(Vector2 lhs, Vector2 rhs)
+		{
+			Vector2 result;
+			result.x = lhs.x + rhs.x;
+			result.y = lhs.y + rhs.y;
+
+			return result;
+		}
+
+		//V = V – V(point translated by a vector)
+		public static Vector2 operator-(Vector2 lhs, Vector2 rhs)
+		{
+			Vector2 result;
+			result.x = lhs.x - rhs.x;
+			result.y = lhs.y - rhs.y;
+
+			return result;
+		}
+
+		//V = V x f(vector scale)
+		public static Vector2 operator*(Vector2 lhs, float rhs)
+		{
+			Vector2 result;
+			result.x = lhs.x * rhs;
+			result.y = lhs.y * rhs;
+
+			return result;
+		}
+
+		//V = f x V(vector scale)
+		public static Vector2 operator *(float lhs, Vector2 rhs)
+		{
+			Vector2 result;
+			result.x = lhs * rhs.x;
+			result.y = lhs * rhs.y;
+
+			return result;
+		}
+	}
+}
